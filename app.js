0a32683e4cf7058d204a455ccca06ac573d58fae
trueberryless <felix@schneider-it.at>
2022-11-06 15:09:43 +0100

    Page looks like Page inside Page

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
-function scrollFooter(bottomscrollY, contentHeight, footerHeight) {
-    var footerContent = document.getElementById("footer-content");
-    if (bottomscrollY > contentHeight) {
-        var transformFooterY = ((bottomscrollY - contentHeight) / footerHeight) * 100;
-        footerContent.style.height = transformFooterY + "%";
+function HeaderTopActive() {
+    let header = document.getElementById("header");
+    if (window.scrollY == 0) {
+        header.setAttribute("top-active", "");
     } else {
-        footerContent.style.height = "100%";
+        header.removeAttribute("top-active");
     }
 }
 
 window.addEventListener("load", (event) => {
-    var windowHeight = window.innerHeight,
-        footerHeight = document.getElementById("footer").clientHeight,
-        contentHeight = document.getElementById("content").clientHeight;
-
-    var bottomscrollY = window.scrollY + windowHeight;
-    scrollFooter(bottomscrollY, contentHeight, footerHeight);
+    HeaderTopActive();
 
     window.addEventListener("scroll", function () {
-        var bottomscrollY = window.scrollY + windowHeight;
-        scrollFooter(bottomscrollY, contentHeight, footerHeight);
-    });
-
-    window.addEventListener("resize", function () {
-        windowHeight = window.innerHeight;
-        footerHeight = document.getElementById("footer").clientHeight;
-        contentHeight = document.getElementById("content").clientHeight;
+        HeaderTopActive();
     });
 });
